249b2d642d9e82ea0eaa1c104028124c74289b80
Huang Rui <bjhuangr@cn.ibm.com>
2018-12-07 09:36:37 +0800

    add neutron-zvm-agent docker file
    Change-Id: I0691d212a2bad8701ffd01fbe46e200999036aeb

--- docker/nova/nova-compute/extend_start.sh
+++ docker/nova/nova-compute/extend_start.sh
@@ -3,3 +3,10 @@
 if [[ ! -d /var/lib/nova/instances ]]; then
     mkdir -p /var/lib/nova/instances
 fi
+
+if [[ ! -d /etc/zvmsdk ]]; then
+    mkdir -p /etc/zvmsdk
+fi
+
+touch /etc/zvmsdk/token.dat
+chmod 755 -R /etc/zvmsdk
